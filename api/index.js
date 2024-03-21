2ee2c8694f55cdcfe6379121d1d9726c246ffa38
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-03-21 22:39:37 +0530

    create signup route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRoute from './reoutes/user.route.js'
+import authRoute from './reoutes/auth.route.js'
 
 console.log(process.env.MONGo_URI);
 const uri = "mongodb+srv://mern:mern@mern.41cokr0.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern"
@@ -11,7 +12,10 @@ mongoose.connect(uri).then(() => {
     console.log("connect to db");
 })
 
+app.use(express.json());
+
 app.use('/api/user', userRoute)
+app.use('/api/auth', authRoute)
 
 
 app.listen(8080, () => {
