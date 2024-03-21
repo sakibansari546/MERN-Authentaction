e30e5b59f8c58294d8896205a5de9df45215d12d
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-03-21 21:59:33 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
 const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.MONGo_URI);
+const uri = "mongodb+srv://mern:mern@mern.41cokr0.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern"
+mongoose.connect(uri).then(() => {
+    console.log("connect to db");
+})
+
+app.get('/', (req, res) => {
+    res.json({ api: "Working" })
+})
 
 app.listen(8080, () => {
     console.log('server is running');
